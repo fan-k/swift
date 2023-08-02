fe15565a90f5d84990fc4fe58550af69bbdf1c95
dubin <dubin@021.com>
2023-08-02 15:56:51 +0800

    闭包缩写函数的参数名

--- Playground_day_1/闭包.playground/Contents.swift
+++ Playground_day_1/闭包.playground/Contents.swift
@@ -25,6 +25,7 @@ let drivieWithReturn = {(place:String) -> String in
 let message = drivieWithReturn("London")
 print(message)
 
+/*
 //闭包当做参数
 func travel (action:()->Void){
     print("I'm getting ready to go.")
@@ -36,4 +37,33 @@ travel(action: drivie)
 //尾随闭包  闭包作为最后一个参数时
 travel {
     print("ssss")
+}
+*/
+/**缩写函数参数*/
+//接收一个参数
+func travel(action :(String) -> String){
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+//调用1
+travel { (place:String) -> String in
+    return "I'm going to \(place) in my car"
+}
+//Swift知道该闭包的参数必须是一个字符串，因此我们可以删除它：
+travel { place -> String in
+    return "I'm going to \(place) in my car"
+}
+//还知道闭包必须返回一个字符串，因此我们可以删除它：
+travel { place in
+    return "I'm going to \(place) in my car"
+}
+//由于闭包只有一行代码，该代码必须是返回值的代码，因此Swift也让我们删除return关键字：
+travel { place in
+    "I'm going to \(place) in my car"
+}
+//Swift有一个速记语法，可以让你走得更短。与其写place in不如让Swift为闭包的参数提供自动名称。这些用美元符号命名，然后是从0开始计数的数字。
+travel {
+    "I'm going to \($0) in my car"
 }
