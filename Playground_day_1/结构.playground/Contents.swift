8d8b8a8b05b23d7b28caf9c163bd4440f32f68f2
dubin <dubin@021.com>
2023-08-02 16:22:32 +0800

    结构体创建

--- Playground_day_1/结构.playground/Contents.swift
+++ Playground_day_1/结构.playground/Contents.swift
@@ -3,4 +3,37 @@ import Cocoa
 var greeting = "Hello, playground"
 //结构体
 
+//结构体和元组的区别：元组没有名字
 
+struct Sport{
+    var name :String
+    var age :Int
+    var isOlympicSport : Bool
+    var olympicStatus :String{
+        if isOlympicSport{
+            return "\(name) is an Olympic sport"
+        }
+        else{
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = Sport(name: "Chessboxing", age:20,isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+
+//didset  willset
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
