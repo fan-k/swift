f8e355560ff3aae1f418f1b43634f7f9df5b6f63
dubin <dubin@021.com>
2023-08-03 16:02:13 +0800

    协议扩展、默认值

--- Playground_day_1/协议.playground/Contents.swift
+++ Playground_day_1/协议.playground/Contents.swift
@@ -22,3 +22,43 @@ protocol Computer: Product {
     var memory: Int { get set }
     var storage: Int { get set }
 }
+
+
+//协议扩展  --swift只允许扩展计算属性 不允许扩展存储属性 ，使用runtime或许可以
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+//Swift的数组和集合都符合一个名为Collection的协议，因此我们可以为该协议编写扩展
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+
+        for name in self {
+            print(name)
+        }
+    }
+}
+//调用
+pythons.summarize()
+beatles.summarize()
+
+//面向协议编程---对声明的协议提供默认方法或数值
+
+//声明一个Identifiable协议
+protocol Identifiable {
+    var id: String { get set }
+    func identify()
+}
+//通过扩展Identifiable 提供默认值
+extension Identifiable {
+    func identify() {
+        print("My ID is \(id).")
+    }
+}
+//当我们创建一个符合Identifiable类型时，会自动获取identify()
+struct User: Identifiable {
+    var id: String
+}
+
+let twostraws = User(id: "twostraws")
+twostraws.identify()
+
